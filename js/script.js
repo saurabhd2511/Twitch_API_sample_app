2c8cc1292bfac91d9cf92a85dc53d211f7379aa6
Saurabh Deshpande <saurabhd2511@gmail.com>
2018-05-15 19:48:23 -0400

    updated html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//just run the jquery on load of document
+$(document).ready(function() {
+	//code camp online status and get status api call
+	var url = "";
+	$.getJSON(url, function(returnData) {
+		
+	});
+});
